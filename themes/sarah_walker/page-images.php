a7b875fe0b77c2430eb8fa8d09e12fd272e679dd
dylanfisher <hi@dylanfisher.com>
2015-06-28 14:44:34 -0400

    Remove lightbox functionality

--- themes/sarah_walker/page-images.php
+++ themes/sarah_walker/page-images.php
@@ -20,18 +20,14 @@ Template Name: Images
         if ( $works->have_posts() ):
           while ( $works->have_posts() ):
             $works->the_post();
-            $post_meta_json = json_encode(get_post_meta($post->ID));
-            $post_meta = htmlentities($post_meta_json);
             $images = get_attached_media('image');
             foreach ( $images as $image ):
-              $image_small = wp_get_attachment_image_src($image->ID, 'small', false);
-              $url_small = $image_small[0];
               $image_medium = wp_get_attachment_image_src($image->ID, 'medium', false);
               $url = $image_medium[0];
               $width = $image_medium[1];
               $height = $image_medium[2];
-              echo '<a href="'.get_permalink().'" data-lightbox data-image-full="'.$image->guid.'" data-title="'.get_the_title().'" data-post-meta="'.$post_meta.'">';
-                echo '<img src="'.$url.'" data-src="'.$url.'" width="'.$width.'" height="'.$height.'">';
+              echo '<a href="'.get_permalink().'">';
+                echo '<img src="'.$url.'" width="'.$width.'" height="'.$height.'">';
               echo '</a>';
             endforeach;
 
--- themes/sarah_walker/single.php
+++ themes/sarah_walker/single.php
@@ -2,12 +2,16 @@
   <div class="content">
     <div id="post-<?php the_ID() ?>" <?php post_class() ?>>
       <?php
+        $images_page = get_page_by_path('images');
+        $images_page = get_permalink($images_page);
         $images = get_attached_media('image');
         foreach ( $images as $image ):
           $attrs = array(
             'data-image-full' => $image->guid
           );
-          echo wp_get_attachment_image($image->ID, 'full', false, $attrs);
+          echo '<a href="'.$images_page.'">';
+            echo wp_get_attachment_image($image->ID, 'full', false, $attrs);
+          echo '</a>';
         endforeach;
       ?>
     </div><!-- .post -->
